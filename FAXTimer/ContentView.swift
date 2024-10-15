e9ad090bf5e4df077e4f936f2c52c10ab5d42131
YMT <tk_nakagawa@yasuhira.com>
2024-10-15 13:26:00 +0900

    Initial Commit

---
+++ FAXTimer/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  FAXTimer
+//
+//  Created by YMT_KIKAKU on 2024/10/15.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ FAXTimer/FAXTimerApp.swift
@@ -0,0 +1,17 @@
+//
+//  FAXTimerApp.swift
+//  FAXTimer
+//
+//  Created by YMT_KIKAKU on 2024/10/15.
+//
+
+import SwiftUI
+
+@main
+struct FAXTimerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
